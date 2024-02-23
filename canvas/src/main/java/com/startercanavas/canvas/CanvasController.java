88a092546c828ef628b46fe20f679cf6d15f4dbe
Justin Pau <justin.pau@sjsu.edu>
2024-02-23 15:31:55 -0800

    setup for springboot app

---
+++ canvas/src/main/java/com/startercanavas/canvas/CanvasController.java
@@ -0,0 +1,20 @@
+package com.startercanavas.canvas;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CanvasController {
+
+    @RequestMapping
+    public String index() {
+        //default path
+        return "welcome to canvas";
+    }
+
+    @RequestMapping("/show")
+    public String show() {
+        //path for http://localhost:8080/show
+        return "Your classes are...";
+    }
+}
---
+++ canvas/src/test/java/com/startercanavas/canvas/CanvasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startercanavas.canvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanvasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
