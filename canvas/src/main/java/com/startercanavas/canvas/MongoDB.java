9e7dadbd0edb7101c138ca8da76a793cf2ed84ab
tommy <tommy.dao@sjsu.edu>
2024-02-27 15:11:03 -0800

    Added MongoDB database connection

---
+++ canvas/src/main/java/com/startercanavas/canvas/MongoDB.java
@@ -0,0 +1,31 @@
+package com.startercanavas.canvas;
+
+import static com.mongodb.client.model.Filters.eq;
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoDB {
+    public static void main( String[] args ) {
+
+        // path to MongoDB client; probably move this later
+        String uri = "mongodb+srv://Tommy:yM0Iah7hRu06ciqC@cmpe202.nducmko.mongodb.net/?retryWrites=true&w=majority";
+
+        //Test database connection using sample data
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("comments");
+
+            Document doc = collection.find(eq("name", "Mercedes Tyler")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
+    }
+}
