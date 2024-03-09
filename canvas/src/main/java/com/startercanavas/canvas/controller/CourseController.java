9262066a02a545c0849a5db416b10cb08674311f
pranavgargsjsu <161090557+pranavgargsjsu@users.noreply.github.com>
2024-03-09 12:35:23 -0800

    Adding logic for Courses
    Adding logic for getting/updating courses

--- canvas/src/main/java/com/startercanavas/canvas/controller/CourseController.java
+++ canvas/src/main/java/com/startercanavas/canvas/controller/CourseController.java
@@ -1,4 +1,25 @@
 package com.startercanavas.canvas.controller;
 
+import com.startercanavas.canvas.model.Course;
+import com.startercanavas.canvas.service.CourseService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+@RestController
+@RequestMapping("/api/v1/courses")
 public class CourseController {
+    @Autowired
+    private CourseService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Course>> getCourse(@PathVariable ObjectId id) {
+        return new ResponseEntity<Optional<Course>> (userService.getCourse(id), HttpStatus.OK);
+    }
 }
--- canvas/src/main/java/com/startercanavas/canvas/repository/CourseRepository.java
+++ canvas/src/main/java/com/startercanavas/canvas/repository/CourseRepository.java
@@ -1,4 +1,14 @@
 package com.startercanavas.canvas.repository;
 
-public interface CourseRepository {
+
+import com.startercanavas.canvas.model.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import com.startercanavas.canvas.model.Course;
+
+@Repository
+public interface CourseRepository extends MongoRepository<Course, ObjectId> {
+
 }
+
